bf42534e47860fa0cc05f847384be1c62e4f5a3c
Jakub Dzidziul <jakub.dzidziul@gmail.com>
2023-07-08 15:22:41 +0200

    finished hamburger menu

---
+++ dist/script.js
@@ -0,0 +1,19 @@
+const navBar = document.querySelector('[data-navbar-menu]')
+const navBarBtn = document.querySelector('[data-navbar-button]')
+
+
+window.addEventListener('resize', () => {
+    if (window.innerWidth >= 700) navBar.classList.remove('mobile')
+})
+
+window.addEventListener('resize', () => {
+    if (window.innerWidth <= 700) navBar.classList.add('mobile')
+})
+
+
+navBarBtn.addEventListener('click', () => {
+    navBar.classList.toggle('left');
+    navBarBtn.classList.toggle('active');
+})
+
+console.lo
